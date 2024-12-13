5a4122823b214b1a934f761d3bc81ad94cd6fd57
Maximus <max@email.com>
2024-12-13 22:29:06 +0300

    Структуры

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-//start
+package main
+
+import (
+	"fmt"
+)
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func main() {
+	myCar := Car{"BMW", 150}
+	myCar.brand = "Ferrari"
+	fmt.Println(myCar.brand, myCar.maxSpeed)
+}
+
+type Car struct {
+	brand    string
+	maxSpeed int
+}
